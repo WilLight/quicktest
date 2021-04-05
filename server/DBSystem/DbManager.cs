1689e3fed2a50c929204f30257ef3a55910974ba
Artem Yurchenko <artem@studiovector.pl>
2021-04-05 12:28:55 +0200

    Simple user registration was implemented

--- server/DBSystem/DbManager.cs
+++ server/DBSystem/DbManager.cs
@@ -50,7 +50,7 @@ namespace server.DBSystem
         /// </summary>
         /// <param name="userCredentials">Credentials from the log in attempt (login and password).</param>
         /// <param name="userData">User data, that belongs to the user data record with the same credentials. (will be null when nothing will be found).</param>
-        /// <returns>Return true for successful search and false when nothing was find.</returns>
+        /// <returns>Returns true for successful search and false when nothing was find.</returns>
         public bool TryLogInUser(UserCredentials userCredentials, out UserData userData)
         {
             var foundedUsers = _usersCollection.Find(userDataRecord => userDataRecord.Credentials == userCredentials);
@@ -62,6 +62,41 @@ namespace server.DBSystem
             userData = foundedUserStream.Current.FirstOrDefault();
 
             return foundedUsers.CountDocuments() != 0;
+        }
+
+        /// <summary>
+        /// Try to register new user with provided credentials.
+        /// </summary>
+        /// <param name="userCredentials">Provided user credentials (login & password).</param>
+        /// <param name="userData">Data of the new user (can be null when validation was failed).</param>
+        /// <returns>Returns true when a new user was successfully registered and false when registration was failed because of data validation.</returns>
+        public bool TryRegisterUser(UserCredentials userCredentials, out UserData userData)
+        {
+            if (TryLogInUser(userCredentials, out _))
+            {
+                userData = null;
+                
+                return false;
+            }
+            
+            userData = new UserData(++_lastUserId, userCredentials);
+            
+            _usersCollection.InsertOne(userData);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Removes one user with the same id.
+        /// </summary>
+        /// <param name="userId">Id of the user, that need to be removed.</param>
+        public void RemoveUser(uint userId)
+        {
+            _usersCollection.DeleteOne(new BsonDocument
+                {
+                    {"_id", userId}
+                }
+            );
         }
 
 #if DEBUG
