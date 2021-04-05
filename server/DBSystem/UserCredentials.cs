c94770836eb5c9774d56221921891279e0d61fe5
Artem Yurchenko <artem@studiovector.pl>
2021-04-05 11:53:36 +0200

    User log in was implemented and tested

---
+++ server/DBSystem/UserCredentials.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace server.DBSystem
+{
+    /// <summary>
+    /// Class, that contains user's log-in data (login & pass).
+    /// </summary>
+    public sealed class UserCredentials
+    {
+        private readonly string _login;
+        private readonly string _password;
+
+        public string Login => _login;
+
+        public string Password => _password;
+
+        public UserCredentials(string login, string password)
+        {
+            _login = login;
+            _password = password;
+        }
+
+        #region Operators
+
+        public static bool operator ==(UserCredentials item1, UserCredentials item2)
+        {
+            return item1?.Login == item2?.Login && item1?.Password == item2?.Password;
+        }
+
+        public static bool operator !=(UserCredentials item1, UserCredentials item2)
+        {
+            return item1?.Login != item2?.Login || item1?.Password != item2?.Password;
+        }
+
+        private bool Equals(UserCredentials other)
+        {
+            return _login == other._login && _password == other._password;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || obj is UserCredentials other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_login, _password);
+        }
+
+        #endregion
+    }
+}
