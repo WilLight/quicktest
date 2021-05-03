d76c1cd9b11abd2defbfe8e80864f9bb900ec040
WilLight <willight.everfor@gmail.com>
2021-05-03 14:44:34 +0200

    Added Dependency Injection of DbManager to Startup

--- server/Startup.cs
+++ server/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using server.DBSystem;
+
 namespace server
 {
     public class Startup
@@ -25,6 +27,7 @@ namespace server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<DbManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
