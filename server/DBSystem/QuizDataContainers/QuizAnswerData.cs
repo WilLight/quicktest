85fc881facf3941b02559172d436ee1a54e79889
WilLight <willight.everfor@gmail.com>
2021-07-04 20:22:37 +0200

    Returned ownerId to Quizzes, fixed database document modification

--- server/DBSystem/QuizDataContainers/QuizAnswerData.cs
+++ server/DBSystem/QuizDataContainers/QuizAnswerData.cs
@@ -9,17 +9,20 @@ namespace server.DBSystem.QuizDataContainers
     public sealed class QuizAnswerData
     {
         public uint QuizId { get; set; }
+        public uint OwnerId { get; set; }
         public List<QuizQuestionData> Questions { get; set; }
 
-        public QuizAnswerData(uint quizId, List<QuizQuestionData> questions)
+        public QuizAnswerData(uint quizId, uint ownerId, List<QuizQuestionData> questions)
         {
             QuizId = quizId;
+            OwnerId = ownerId;
             Questions = questions;
         }
 
         public QuizAnswerData(QuizAnswerData anotherQuiz)
         {
             QuizId = anotherQuiz.QuizId;
+            OwnerId = anotherQuiz.OwnerId;
             Questions = anotherQuiz.Questions;
         }
     }
